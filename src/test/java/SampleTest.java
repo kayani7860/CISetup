cecf39c3cc213edc96808a8ea2de8762e70bd999
Hammad Kayani <36533976+kayani7860@users.noreply.github.com>
2021-04-01 02:31:11 +0200

    First method Sum and first Test Sum

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,17 @@
+import org.junit.*;
+
+
+public class SampleTest {
+	@Test
+	public void testSum() {
+		// Given
+		Calculator calculator = new Calculator();
+		// When
+		int result = calculator.sum(2, 2);
+		// Then
+		if (result != 4) {   // if 2 + 2 != 4
+			Assert.fail();
+		}
+	}
+
+}
